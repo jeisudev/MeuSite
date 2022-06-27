9465f342972f8a5b95f282258e92ad84b1d9a191
jeisudev <jeisonrothmund97@gmail.com>
2022-06-27 14:06:18 -0300

    anexo

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,9 @@
 import {ElementPosiition} from './style';
 import {IntroText} from '../../Components/TextAndIntro/index';
-import {ButtonSite} from '../../Components/Button/index'
 
 export function HomePage(){
     return(
         <div>
-            <ButtonSite name='Bts'/>
             <ElementPosiition>
                 <IntroText name='Jeison Rothmund' profission='Dev Front-end'/>
             </ElementPosiition>
